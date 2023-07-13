715b9e323ba6d023414d54a5dbe5813018208aa3
Jônatas Afonso <jonatas.afonso@gmail.com>
2023-07-13 21:52:38 +0100

    Final da aula

--- src/Gandalf.BackOffice/Gandalf.BackOffice/script/geral.js
+++ src/Gandalf.BackOffice/Gandalf.BackOffice/script/geral.js
@@ -20,7 +20,7 @@ function UsuarioLogado() {
     var usuario = document.cookie;
     if (usuario == "") {
         window.location.replace("index.html");
-    }    
+    }
 }
 
 // Para apresentar alguns dados relevantes no dashboard
@@ -34,4 +34,101 @@ function ObterAlertas() {
     document.getElementById("spanAlerta01").innerHTML = alertas[0];
     document.getElementById("spanAlerta02").innerHTML = alertas[1];
     document.getElementById("spanAlerta03").innerHTML = alertas[2];
+}
+
+
+function ObterEstatisticas() {
+    var est01 = {
+        Quantidade: 10,
+        Label: "Pedidos",
+        Icone: "pedidos.jpg",
+        Link: "pedidos.html",
+        Cor: "#007bff"
+    };
+
+    var est02 = {
+        Quantidade: 8,
+        Label: "Clientes Novos",
+        Icone: "clientes.jpg",
+        Link: "clientes.html",
+        Cor: "green"
+    };
+    var est03 = {
+        Quantidade: 100,
+        Label: "Estoque",
+        Icone: "estoques.jpg",
+        Link: "estoques.html",
+        Cor: "orange"
+    };
+    var est04 = {
+        Quantidade: 10,
+        Label: "Produtos Novos",
+        Icone: "produtos.jpg",
+        Link: "produtos.html",
+        Cor: "yellow"
+    };
+
+    var estatisticas = [
+        est01, est02, est03, est04
+    ];
+
+    FormatarEstatisticas("estatistica01", estatisticas[0]);
+    FormatarEstatisticas("estatistica02", estatisticas[1]);
+    FormatarEstatisticas("estatistica03", estatisticas[2]);
+    FormatarEstatisticas("estatistica04", estatisticas[3]);
+}
+
+
+function FormatarEstatisticas(elemento, dados) {
+    var elemento = document.getElementById(elemento);
+    if (elemento == null) {
+        return;
+    };
+
+    elemento.innerHTML = "<table style='background-color:" + dados.Cor + "'><tr><td>" + dados.Label
+        + "</td><td>" + dados.Icone
+        + "</td></tr><tr><td>Quantidade</td><td>" + dados.Quantidade + "</td></tr>"
+        + "<tr><td colspan='2'> <a href='" + dados.Link + "'>Maiores Informações</a> </td></tr></table>";
+}
+
+
+function ObterClientes() {
+    var clientes = [];
+
+    for (var i = 0; i < 10; i++) {
+        var cliente = {
+            id: i,
+            nome: "Cliente 0" + i,
+            endereco: "Rua 0" + i,
+            dataNascimento: "01/01/2001",
+            email: "cliente" + i + "@email.com",
+            cidade: "Cidade 0" + i,
+            nif: "123456789"
+        };
+
+        clientes[i] = cliente;
+    }
+
+    formatarClientes(clientes);
+
+}
+
+var formatarClientes = function (clientes) {
+    var elemento = document.getElementById("tblClientes");
+    var cabecalho = "<tbody><tr><th>Id</th><th>Nome</th><th>Endereço</th><th>Data de Nascimento</th><th>Email</th><th>Cidade</th><th>NIF</th></tr>";
+    var corpo = "";
+
+    for (var i = 0; i < clientes.length; i++) {
+        corpo += "<tr><td>" + clientes[i].id + "</td>" +
+            "<td>" + clientes[i].nome + "</td>" +
+            "<td>" + clientes[i].endereco + "</td>" +
+            "<td>" + clientes[i].dataNascimento + "</td>" +
+            "<td>" + clientes[i].email + "</td>" +
+            "<td>" + clientes[i].cidade + "</td>" +
+            "<td>" + clientes[i].nif + "</td></tr>";
+    }
+
+    var rodape = "</tbody>";
+
+    elemento.innerHTML = cabecalho + corpo + rodape;
 }
